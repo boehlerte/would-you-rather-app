8bca2d429ea773fde243b35478828caac9925120
Erin Boehlert <eringraceboehlert@gmail.com>
2019-03-30 15:20:49 -0600

    view poll button now routes to poll page

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatQuestion } from '../utils/helpers';
+import { withRouter } from 'react-router-dom'
 
 class Question extends Component {
+
+    toPoll = (e, id) => {
+        e.preventDefault()
+        this.props.history.push(`/questions/${id}`)
+    }
+
     render() {
         const { question } = this.props
+        console.log('question', question)
         return (
             <div className='question'>
                 <div className='row header'>
@@ -17,7 +25,10 @@ class Question extends Component {
                     <div className='options col-8'>
                         <b>Would you rather</b>
                         <div>...{question.optionOne.text.substring(0, 15)}...</div>
-                        <button className='btn btn-outline-primary view-poll'>View Poll</button>
+                        <button 
+                            className='btn btn-outline-primary view-poll'
+                            onClick={(e) => this.toPoll(e, question.id)}
+                        >View Poll</button>
                     </div>
                 </div>
             </div>
@@ -36,4 +47,4 @@ function mapStateToProps({authedUser, questions, users}, {id}) {
     }
 }
 
-export default connect(mapStateToProps)(Question)
+export default withRouter(connect(mapStateToProps)(Question))
