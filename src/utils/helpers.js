b781abc98c505b8c1eb378a66453078162be25c8
Erin Boehlert <eringraceboehlert@gmail.com>
2019-04-10 18:17:46 -0600

    minor bug fix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,8 @@
 export function formatQuestion (questions, users, question, authedUser) {
   const { id, optionOne, optionTwo } = question
   const { name, avatarURL } = users[question.author]
-
   const isOptionOne = questions[id].optionOne.votes.includes(authedUser);
-  const isOptionTwo = questions[id].optionOne.votes.includes(authedUser);
+  const isOptionTwo = questions[id].optionTwo.votes.includes(authedUser);
   let answer;
   if (isOptionOne) {
     answer = 'optionOne';
